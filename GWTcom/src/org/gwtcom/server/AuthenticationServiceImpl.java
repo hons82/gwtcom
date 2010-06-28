52d38045e64f187284846f145da8368bb3eca594
hons82 <hons82@b32dfa24-13a5-46d3-4056-ccc0b38b9b25>
2010-06-28 15:08:25 +0000

    Fixed the authentication problem. Thanks to Daniel

--- GWTcom/src/org/gwtcom/server/AuthenticationServiceImpl.java
+++ GWTcom/src/org/gwtcom/server/AuthenticationServiceImpl.java
@@ -12,19 +12,27 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service("authenticationService")
 public class AuthenticationServiceImpl implements AuthenticationService {
 
 	public boolean authenticate(String username, String password) {
+		System.out.println(">>>>> Authenticate called");
 		// creating an authenticated user token for demo
 		// regardless of username and password values
 		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
-		User user = new User("xxx", "yyy", true, true, true, true, authorities);
+		User user = new User("root", "root", true, true, true, true, authorities);
 		Authentication auth = new UsernamePasswordAuthenticationToken(user, password, authorities);
 		SecurityContext sc = new SecurityContextImpl();
+
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		attr.getRequest().getSession().setAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY, username);
+
 		sc.setAuthentication(auth);
 		SecurityContextHolder.setContext(sc);
 
@@ -32,13 +40,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 	}
 
 	public void logout() {
+		System.out.println(">>>>> Logout called");
 		SecurityContextHolder.clearContext();
 	}
 
 	@Override
 	public boolean isLoggedIn() {
 		System.out.println(">>>>> AuthenticationService.isLoggedIn -> " + SecurityContextHolder.getContext().getAuthentication());
-		if (SecurityContextHolder.getContext().getAuthentication() != null) {
+		// TODO: That's maybe not enough of security 
+		if (!SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser")) {
 			return true;
 		}
 		return false;
